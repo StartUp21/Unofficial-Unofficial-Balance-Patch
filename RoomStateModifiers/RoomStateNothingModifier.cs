07f62e5569d50cb73f9c1cb986c5a523c9bbeb08
StartUp21 <simo210302@gmail.com>
2025-06-05 01:53:45 +0200

    Fixed queen having room ability preemptively.

---
+++ RoomStateModifiers/RoomStateNothingModifier.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unofficial_Unofficial_Balance_Patch.RoomStates
+{
+	public class RoomStateNothingModifier : RoomStateModifierBase
+	{
+	}
+}
