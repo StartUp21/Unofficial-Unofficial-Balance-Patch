68a04749fe5f348cdf2829adaa34a3cd30fbd916
Vu Duc Nhat Minh <professionalmothlover2511@gmail.com>
2025-06-11 07:26:43 +0700

    Added Crucible Extension

---
+++ RoomStateModifiers/RoomStateCapacityModifier.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unofficial_Unofficial_Balance_Patch.RoomStateModifiers
+{
+    internal class RoomStateCapacityModifier : RoomStateModifierBase, IRoomStateCapacityModifier
+    {
+        private int capacity;
+        public override void Initialize(RoomModifierData roomModifierData, ICoreGameManagers coreGameManagers)
+        {
+            base.Initialize(roomModifierData, coreGameManagers);
+            capacity = roomModifierData.GetParamInt();
+        }
+        public int GetModifiedCapacity()
+        {
+            return this.capacity;
+        }
+    }
+}
