72dcb9d73ccc0ef83c3394f37b351236b844780b
StartUp21 <simo210302@gmail.com>
2025-06-16 23:23:17 +0200

    bolete, mostly

--- Plugin.cs
+++ Plugin.cs
@@ -1,24 +1,29 @@
 ﻿using BepInEx;
 using HarmonyLib;
+using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
 using TrainworksReloaded.Base;
+using TrainworksReloaded.Base.Card;
 using TrainworksReloaded.Base.CardUpgrade;
+using TrainworksReloaded.Base.Effect;
+using TrainworksReloaded.Base.Extensions;
+using TrainworksReloaded.Base.Prefab;
+using TrainworksReloaded.Base.Room;
 using TrainworksReloaded.Core;
 using TrainworksReloaded.Core.Extensions;
 using TrainworksReloaded.Core.Impl;
 using TrainworksReloaded.Core.Interfaces;
+using UnityEngine;
 
 namespace Unofficial_Unofficial_Balance_Patch
 {
-
+	[HarmonyPatch]
 	[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
 	public class Plugin : BaseUnityPlugin
 	{
-		// private readonly Dictionary<string, FileInfo> _jsonFiles = new Dictionary<string, FileInfo>();
 		private void Awake()
 		{
 			var assemblyFolder = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
@@ -27,20 +32,21 @@ namespace Unofficial_Unofficial_Balance_Patch
 			Railhead.GetBuilder().Configure(PluginInfo.PLUGIN_GUID, c =>
 			{
 				c.AddMergedJsonFile(GetJsonFilesInDirectory(new DirectoryInfo(Path.Combine(assemblyFolder, "balance_patches")))
-					.Select(x => Path.GetRelativePath(assemblyFolder, x.FullName)).ToList());
+				.Select(x => Path.GetRelativePath(assemblyFolder, x.FullName)).ToList());
 			});
+			/*
 			Railend.ConfigurePostAction(c =>
 			{
 				if (c.GetInstance<GameDataClient>().TryGetProvider<SaveManager>(out var saveManager))
 				{
-					/*
+					
 					var upgrades = c.GetInstance<PluginAtlas>().PluginDefinitions[PluginInfo.PLUGIN_GUID].Configuration
 						.GetSection("upgrades")?
 						.GetChildren()
 						.Select(x => x.GetSection("id").ParseString())
 						.Cast<string>().ToList() ?? new List<string>();
-					*/
-					/*
+					
+					
 					var allGameData = saveManager.GetAllGameData();
 					foreach (var upgradeTree in allGameData.GetAllClassDatas()
 						.SelectMany(x => (List<ChampionData>)AccessTools.Field(typeof(ClassData), "champions").GetValue(x))
@@ -55,35 +61,11 @@ namespace Unofficial_Unofficial_Balance_Patch
 								list[i] = data;
 							}
 						}
-					}*/
-				}
-			});
-			new Harmony(PluginInfo.PLUGIN_GUID + "-Harmony").PatchAll(Assembly.GetExecutingAssembly());
-			/*
-			Railend.ConfigurePostAction(c =>
-			{
-				if (c.GetInstance<GameDataClient>().TryGetProvider<SaveManager>(out var saveManager))
-				{
-					var allGameData = saveManager.GetAllGameData();
-					CharacterData impQueenUnit = allGameData.FindCardDataByName("ImpQueenChampion").GetSpawnCharacterData();
-					// AccessTools.Field(typeof(CharacterData), "attackDamage")
-					// 	.SetValue(impQueenUnit, 5);
-					// AccessTools.Field(typeof(CharacterData), "unitAbility")
-					// 	.SetValue(impQueenUnit,	allGameData.FindCardDataByName("UUBP_ImpQueenChampionAbility".ToKey("Card")));
-					List<CardUpgradeTreeData.UpgradeTree> newUpdatedTree = new List<CardUpgradeTreeData.UpgradeTree>();
-					CardUpgradeTreeData impQueenChampionUpgradeTree = allGameData.FindClassData("c595c344-d323-4cf1-9ad6-41edc2aebbd0").GetUpgradeTree(1);
-					foreach (var upgradeTree in impQueenChampionUpgradeTree.GetUpgradeTrees())
-					{
-						var newTree = new CardUpgradeTreeData.UpgradeTree();
-						newUpdatedTree.Add(newTree);
-						AccessTools.Field(typeof(CardUpgradeTreeData.UpgradeTree), "cardUpgrades").SetValue(upgradeTree,
-							upgradeTree.GetCardUpgrades()
-							.Select(x => allGameData.GetAllCardUpgradeData().LastOrDefault(y => y.name == $"UUBP_{x.name}".ToKey("Upgrade")) ?? x)
-							.Cast<CardUpgradeData>().ToList());
 					}
 				}
 			});
 			*/
+			new Harmony(PluginInfo.PLUGIN_GUID + "-Harmony").PatchAll(Assembly.GetExecutingAssembly());
 		}
 		private IEnumerable<FileInfo> GetJsonFilesInDirectory(DirectoryInfo directory)
 		{
